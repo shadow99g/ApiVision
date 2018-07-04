62bc6cf900227cf48b7051a8325a690e3fe2689f
Jordy Valenzuela <31623547+shadow99g@users.noreply.github.com>
2018-07-04 16:22:54 -0500

    Add files via upload

---
+++ ApiVision_v0.0.2/src/java/dao/dao.java
@@ -0,0 +1,101 @@
+package dao;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import model.model;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class dao {
+    public model consultarapiVision(model dl) throws JSONException, IOException {
+        HttpClient httpClient = new DefaultHttpClient();
+        
+
+        try {
+            HttpPost request = new HttpPost("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBOd2BpX9qpj0O0C9t9yy6SVOY8f3W04Dg");// creamos la conexion con el API
+            StringEntity params = new StringEntity("{\n" //Creamos los parametros a mandar
+                    + "  \"requests\":[\n" //representa una solicitud de recursos.
+                    + "    {\n"
+                    + "      \"image\":{\n" 
+                    + "        \"source\":{\n" 
+                    + "          \"imageUri\":\n" //Ruta del archivo
+                    + "            \"gs://jvalenzuela/"+dl.getNameFile()+"\"\n" //Agregamos el Nombre del Archivo
+                    + "        }\n"
+                    + "      },\n"
+                    + "      \"features\":[\n" //Definimos las caracteristicas 
+                    + "        {\n"
+                    + "          \"type\":\"DOCUMENT_TEXT_DETECTION\",\n" //Tipo de Api a Utilizar
+                    + "          \"maxResults\":1\n" 
+                    + "        }\n"
+                    + "      ]\n"
+                    + "    }\n"
+                    + "  ]\n"
+                    + "}");
+            
+            request.addHeader("Content-Type", "application/json"); // agrega un nuevo encabezado HTML que le decimos sera un tipo : aplicacion json
+            request.addHeader("Authorization", "Bearer " + dl.getToken_acces());// agrega un nuevo encabezado HTML el cual sera de Autorizacion : Bearer Token y añade el Token
+            request.setEntity(params); // Obtiene la solicitud de recursos
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+//            System.out.println(EntityUtils.toString(entity));
+
+//            JSONObject root = new JSONObject(EntityUtils.toString(entity));
+//            JSONArray responses = root.getJSONArray("responses");
+//            JSONObject primerElemento = responses.getJSONObject(0);
+//
+//            JSONObject Fulltext = primerElemento.getJSONObject("fullTextAnnotation");
+//
+//            String texto = Fulltext.getString("text");
+//
+//            System.out.println(texto);
+            JSONObject json = new JSONObject(EntityUtils.toString(entity));
+//            System.out.println(json.getJSONArray("responses").getJSONObject(0).getJSONObject("fullTextAnnotation").getString("text"));
+            
+            dl.setResponses(json.getJSONArray("responses").getJSONObject(0).getJSONObject("fullTextAnnotation").getString("text"));
+        } catch (IOException ex) {
+            throw ex;
+        }
+        return dl;
+    }
+    
+    public void envioStorage() {
+        try {
+            //Leo fichero
+            String filename = "C:/Users/PC/Desktop/acta.jpg";
+
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String lineaTotal = "";
+            String linea = reader.readLine();
+            while (linea != null) {
+                lineaTotal = lineaTotal + linea;
+                linea = reader.readLine();
+            }
+
+            String envio = lineaTotal;
+            System.out.println("TAMAÑO KILOBYTES: " + (lineaTotal.length() / 1024));
+            reader.close();
+            StringEntity params = new StringEntity(envio);
+
+            HttpClient httpClient = new DefaultHttpClient();
+            model dl = new model();
+
+            HttpPost request = new HttpPost("https://www.googleapis.com/upload/storage/v1/b/sleyva18/o?uploadType=media&name=acta");
+            request.addHeader("Content-Type", "image/jpeg");
+            request.addHeader("Authorization", "Bearer ya29.c.El_GBSmWiowoXl2KoJh0y9PCpmlYJNbAOsZf7Bsc13KXMRww8n8MSOYFGRlq71IZggBZA9vSVBbp8VH-daR6UDE5iqwWhR4j6J8pqnajH-oTMCo_6eph5ChMo261FjqvuQ");
+            request.setEntity(params);
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+            System.out.println(EntityUtils.toString(entity));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ ApiVision_v0.0.2/src/java/model/model.java
@@ -0,0 +1,35 @@
+package model;
+
+import org.primefaces.model.UploadedFile;
+
+public class model {
+    private String token_acces="ya29.c.El_uBWruxeng6FnO_WUwVSsCLGNAiz3Jq0O1NhD3X0VtW6_GM7qvaZOiaBI8a4n0JrVzlUJJCh1o7GmUst0rEm9otsLp0OTmMTM2PfOGWHljgn_B-bWWQy76UzptFN2PNQ";
+    private String responses;
+    private String nameFile;
+    
+
+    public String getToken_acces() {
+        return token_acces;
+    }
+
+    public void setToken_acces(String token_acces) {
+        this.token_acces = token_acces;
+    }
+
+    public String getResponses() {
+        return responses;
+    }
+
+    public void setResponses(String responses) {
+        this.responses = responses;
+    }
+
+    public String getNameFile() {
+        return nameFile;
+    }
+
+    public void setNameFile(String nameFile) {
+        this.nameFile = nameFile;
+    }
+    
+}
